aece231a8d2346776cf116f070e989ea5b9652e9
Daniel Saidi <daniel.saidi@gmail.com>
2025-01-15 11:25:51 +0100

    Update package scripts

--- package_version.sh
+++ package_version.sh
@@ -4,9 +4,7 @@
 # This script creates a new project version for the package.
 # You can pass in a BRANCH to not use the default git branch.
 
-NAME="AppIconKit"
 DEFAULT_BRANCH="main"
 BRANCH=${1:-$DEFAULT_BRANCH}
-SCRIPT="scripts/version.sh"
-chmod +x $SCRIPT
-bash $SCRIPT $NAME $BRANCH
+SCRIPT="scripts/package_version.sh"
+chmod +x $SCRIPT && bash $SCRIPT $BRANCH
